7ec5703d3a507bc8a8e6f4728fedf698db6e892c
F1LT3R <al@pwn.io>
2016-10-26 12:21:54 -0400

    Beging adding template compilation.

--- mod/markdown.mod.js
+++ mod/markdown.mod.js
@@ -4,40 +4,59 @@ const Promise = require('bluebird');
 const Handlebars = require('handlebars');
 const marked = require('marked');
 
-const markdownToHtml = markdownText => {
-  return new Promise((resolve, reject) => {
-    marked(markdownText, (err, data) => {
-      if (err) {
-        return reject(err);
-      }
-
-      resolve(data);
-    });
-  });
-};
-
+// const compileTemplate =
 
 markserv(plugin => {
   // console.log(plugin);
 
-  return requestPath => {
+  const markdownToHtml = markdownText => {
+    return new Promise((resolve, reject) => {
+      marked(markdownText, (err, html) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(html);
+      });
+    });
+  };
+
+  const compileTemplate = markDownHtml => {
+    return new Promise(resolve => {
+      const model = {
+        markdown: markDownHtml,
+        markserv: plugin.Markconf
+      };
+      const template = Handlebars.compile(plugin.template);
+      const result = template(model);
+      resolve(result);
+    });
+  };
+
+  const preparePayload = compiledHtml => {
+    return new Promise(resolve => {
+      const payload = {
+        statusCode: 200,
+        contentType: 'text/html',
+        data: compiledHtml
+      };
+      resolve(payload);
+    });
+  };
+
+  return request => {
     return new Promise((resolve, reject) => {
       // console.log(plugin.meta);
 
-      markserv.fs.readfile(requestPath)
+      markserv.fs.readfile(request)
       .then(markdownToHtml)
-      .then(html => {
-        const payload = {
-          statusCode: 200,
-          contentType: 'text/html',
-          data: html
-        };
-
+      .then(compileTemplate)
+      .then(preparePayload)
+      .then(payload => {
         resolve(payload);
-      }).catch(err => {
-
+      })
+      .catch(err => {
+        reject(err);
       });
-
     });
   };
 });
